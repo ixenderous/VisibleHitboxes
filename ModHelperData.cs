33c4e1737fe8654c7b799affbc718014f0be6a51
doombubbles <obsidiantoolzg@gmail.com>
2022-07-12 21:25:46 -0700

    Initial Commit

---
+++ ModHelperData.cs
@@ -0,0 +1,12 @@
+﻿namespace TemplateMod;
+
+public static class ModHelperData
+{
+    public const string Version = "1.0.0";
+    public const string Name = "TemplateMod";
+
+    public const string Description = "An empty mod";
+
+    public const string RepoOwner = ""; // TODO add your github username hero
+    public const string RepoName = ""; // TODO add your repo name here
+}
---
+++ TemplateMod.cs
@@ -0,0 +1,18 @@
+﻿global using MelonLoader;
+global using BTD_Mod_Helper.Extensions;
+global using System.Linq;
+using BTD_Mod_Helper;
+using TemplateMod;
+
+[assembly: MelonInfo(typeof(TemplateMod.TemplateMod), ModHelperData.Name, ModHelperData.Version, ModHelperData.RepoOwner)]
+[assembly: MelonGame("Ninja Kiwi", "BloonsTD6")]
+
+namespace TemplateMod;
+
+public class TemplateMod : BloonsTD6Mod
+{
+    public override void OnApplicationStart()
+    {
+        ModHelper.Msg<TemplateMod>("TemplateMod loaded!");
+    }
+}
