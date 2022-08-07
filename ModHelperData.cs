ed3f84a7a10fec8666cb5bdcc4da0e8d064106ef
doombubbles <obsidiantoolzg@gmail.com>
2022-08-07 15:57:28 -0700

    Fancier readme

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -3,10 +3,10 @@
 public static class ModHelperData
 {
     public const string Version = "1.0.0";
-    public const string Name = "Template Mod";
+    public const string Name = "TemplateMod";
 
     public const string Description = "An empty mod";
 
-    public const string RepoOwner = ""; // TODO add your github username hero
-    public const string RepoName = ""; // TODO add your repo name here
+    public const string RepoOwner = ""; // TODO add your github username hero, also in the download url in README.md
+    public const string RepoName = ""; // TODO add your repo name here, also in the download url in README.md
 }
