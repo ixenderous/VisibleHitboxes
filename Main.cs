51759c98814ce050670cd6aa28f074b4594c8a73
TheMegax <mathias.tello12@gmail.com>
2022-11-08 20:14:04 -0300

    Initial commit

---
+++ Main.cs
@@ -0,0 +1,17 @@
+﻿using BTD_Mod_Helper;
+using HitboxMod;
+using MelonLoader;
+using Main = HitboxMod.Main;
+
+[assembly: MelonInfo(typeof(Main), ModHelperData.Name, ModHelperData.Version, ModHelperData.RepoOwner)]
+[assembly: MelonGame("Ninja Kiwi", "BloonsTD6")]
+
+namespace HitboxMod;
+
+public class Main : BloonsTD6Mod
+{
+    public override void OnApplicationStart()
+    {
+        ModHelper.Msg<Main>("TemplateMod loaded!");
+    }
+}
--- ModHelperData.cs
+++ ModHelperData.cs
@@ -1,11 +1,11 @@
-﻿namespace TemplateMod;
+﻿namespace HitboxMod;
 
 public static class ModHelperData
 {
     public const string Version = "1.0.0";
-    public const string Name = "TemplateMod";
+    public const string Name = "HitboxMod";
 
-    public const string Description = "An empty mod";
+    public const string Description = "Shows hitboxes";
 
     public const string RepoOwner = ""; // TODO add your github username hero, also in the download url in README.md
     public const string RepoName = ""; // TODO add your repo name here, also in the download url in README.md
