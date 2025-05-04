fad49f974cca4dd9aed76f309c8557c46f2e657c
ixenderous <ixenderous@gmail.com>
2025-05-04 20:47:37 +1200

    Updated version

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -2,7 +2,7 @@
 
 public static class ModHelperData
 {
-    public const string WorksOnVersion = "48.1";
+    public const string WorksOnVersion = "47.0";
     public const string Version = "1.0.0";
     public const string Name = "VisibleHitboxes";
     public const string Description = "Adds toggleable overlays for bloons, towers, paths, maps, and projectiles, as well as a full map rendering toggle.";
