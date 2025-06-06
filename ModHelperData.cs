4786883829ec07d06c34c3ed9e4976733130ec52
ixenderous <ixenderous@gmail.com>
2025-06-07 01:16:16 +1200

    Updated version

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -3,7 +3,7 @@
 public static class ModHelperData
 {
     public const string WorksOnVersion = "47.0";
-    public const string Version = "1.0.1";
+    public const string Version = "1.0.2";
     public const string Name = "VisibleHitboxes";
     public const string Description = "Adds toggleable overlays for bloons, towers, paths, maps, and projectiles, as well as a full map rendering toggle.";
     public const string RepoOwner = "iXendeRouS";
