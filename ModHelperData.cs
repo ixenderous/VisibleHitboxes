24c357cc94d60cc45fc2c102ce287ed8a6fe5e8e
ixenderous <ixenderous@gmail.com>
2025-11-01 13:48:14 +1300

    Updated version

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -3,7 +3,7 @@
 public static class ModHelperData
 {
     public const string WorksOnVersion = "51.0";
-    public const string Version = "2.0.0";
+    public const string Version = "2.0.1";
     public const string Name = "VisibleHitboxes";
     public const string Description = "Toggles map rendering and enables map and tower outlines when placing.";
     public const string RepoOwner = "ixenderous";
