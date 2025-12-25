0233250be96f2d9f069883b911f26307659e6900
ixenderous <ixenderous@gmail.com>
2025-12-25 23:18:59 +1300

    Updated version and description

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -3,9 +3,9 @@
 public static class ModHelperData
 {
     public const string WorksOnVersion = "52.0";
-    public const string Version = "2.0.2";
+    public const string Version = "2.1.0";
     public const string Name = "VisibleHitboxes";
-    public const string Description = "Toggles map rendering and enables map and tower outlines when placing.";
+    public const string Description = "Adds tower, map, path, bloon, and projectile hitboxes/overlays.";
     public const string RepoOwner = "ixenderous";
     public const string RepoName = "VisibleHitboxes";
 }
